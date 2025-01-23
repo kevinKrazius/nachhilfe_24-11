196b49fa77bf84f1b6ef7f31ab3e9be1c5b0757c
Stitch <dustin.poetzsch@tn.techstarter.de>
2025-01-23 15:08:27 +0100

    fix_Marcus

--- python_flask.py
+++ python_flask.py
@@ -1,9 +1,3 @@
 # Das ist ein Kommentar von Kevin
 # Das ist mein Kommentar von Dustin!
-<<<<<<< HEAD
 # Ein alter Test.
-=======
-# Ein neuer Test
-# Ich lösche alles.
-
->>>>>>> 7c991503c195311a849a9bf92b305570e36d545a
