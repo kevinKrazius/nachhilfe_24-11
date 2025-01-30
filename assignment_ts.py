a8eebcc59c14b231683040669f28efbc045d0b46
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-30 19:03:25 +0100

    Update assignment_ts.py

--- assignment_ts.py
+++ assignment_ts.py
@@ -1,24 +1,29 @@
 def begruessung (name): #fehlende Klammer
     print("Hallo,"  + name) # name gross geschrieben
+begruessung("Max") # fehlender Aufruf der Funktion
 
 def addiere_zahlen(a, b): # fehlender :
-    ergebnis = a + b
+    ergebnis = a + b # fehlender wert
     return ergebnis # fehlender Buchstabe n
 
+
 def subtrahiere_zahlen(a, b):
-    return a - b # falsche Variable
+    
+    return a -b  # falsche Variable
+
 
 def main(): # fehlender :
-    zahl1 = input("Gib eine Zahl ein: ")  
-    zahl2 = input("Gib eine weitere Zahl ein: ")  
+    zahl1 = int(input("Gib eine Zahl ein: "))  #fehlende Umwandlung in eine Zahl
+    zahl2 = int(input("Gib eine weitere Zahl ein: "))  #fehlende Umwandlung in eine Zahl
 
     summe = addiere_zahlen(zahl1, zahl2)  
-    print("Die Summe ist: " + summe)  
+    print("Die Summe ist: ", summe)  # Zahl und string können nicht mit + verknüpft werden
 
     differenz = subtrahiere_zahlen(zahl1, zahl2)  
-    print("Die Differenz ist: " + differenz)  
+    print("Die Differenz ist: ", differenz)  # Zahl und string können nicht mit + verknüpft werden
 
-    begruessung("Max")  
+      
 
 if __name__ == "__main__": # = ist eine Zuweisung.  Vergleich benötigt ==
     main()
+   
