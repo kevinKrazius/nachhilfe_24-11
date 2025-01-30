c9f382b9e44e5451131e1f96e91bffab1b8aaf7d
Marcus622 <marcus.vix@tn.techstarter.de>
2025-01-30 14:16:54 +0100

    Create assignment_ts.py

---
+++ assignment_ts.py
@@ -0,0 +1,24 @@
+def begruessung name:
+print("Hallo, " + Name)
+
+def addiere_zahlen(a, b)
+    ergebnis = a + b
+    return ergebis 
+
+def subtrahiere_zahlen(a, b):
+    return a - c
+
+def main()
+    zahl1 = input("Gib eine Zahl ein: ")  
+    zahl2 = input("Gib eine weitere Zahl ein: ")  
+
+    summe = addiere_zahlen(zahl1, zahl2)  
+    print("Die Summe ist: " + summe)  
+
+    differenz = subtrahiere_zahlen(zahl1, zahl2)  
+    print("Die Differenz ist: " + differenz)  
+
+    begruessung("Max")  
+
+if __name__ = "__main__":
+    main()
