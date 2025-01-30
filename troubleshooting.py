a013e57dcde554980d824a0bc37b2dbb2a458d76
kevinKrazius <kevin.krazius@techstarter.de>
2025-01-30 14:43:37 +0100

    Aufgabe gelöst

--- troubleshooting.py
+++ troubleshooting.py
@@ -1,1 +1,36 @@
 # Nachhilfe Session 30.01.25
+
+def summe(a, b):
+    return a + b
+
+print(summe(1,5))
+
+
+
+def sayHello(vorname, nachname):
+    print(f"Hallo {vorname}, {nachname}" + " " + vorname)
+
+sayHello("Vorname", "Nachname")
+
+
+
+
+def multipliziere(a, b):
+    return a * b
+
+print(multipliziere(2, 3))
+
+
+
+
+def quadrat(x): 
+    return x * x
+
+print(quadrat(4))
+print(quadrat(10))
+
+
+
+
+if 5 > 3:
+    print("Fünf ist größer als drei")
